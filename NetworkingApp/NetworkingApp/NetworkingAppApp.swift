f22df3cfef5206c9357459351023b122911f6691
Mariam Babutsidze <mbabutsidze@tbcbank.com.ge>
2023-04-24 18:12:45 +0400

    adds app

---
+++ NetworkingApp/NetworkingApp/NetworkingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingAppApp.swift
+//  NetworkingApp
+//
+//  Created by Mariam Babutsidze on 24.04.23.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
